b99a6e35414a2fdc8207e1dcaa5b74523dbe4dea
Allen <zifeng.allen@gmail.com>
2023-06-23 00:42:13 +0800

    revert game changes

--- src/App.tsx
+++ src/App.tsx
@@ -28,120 +28,120 @@ function App() {
     // const [user, setUser] = useState("0")
     // const value = {user, setUser}
 
-    const PrivateWrapper = ({ child }: { child: JSX.Element }) => {
+    const PrivateWrapper = ({child}: { child: JSX.Element }) => {
         // const _user = useContext(UserContext).value.user;
         const ua = new UAParser(window.navigator.userAgent)
         const isMobile = ua.getDevice().type === 'mobile'
         if (isMobile) {
-            return <Navigate to="/" />
+            return <Navigate to="/"/>
         }
         const cookie = document.cookie.split(';').map(x => x.trim())
         const token = cookie.find(x => x.startsWith('token='))
         console.log(token)
-        return token !== undefined ? child : <Navigate to="/" />
+        return token !== undefined ? child : <Navigate to="/"/>
     }
 
     // return <UserContext.Provider value={{value}}>
     return <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />}></Route>
-                <Route path="home/" element={<PrivateWrapper child={<Home />} />}></Route>
-                <Route path="game1/" element={<PrivateWrapper child={<Game day={1} />} />}></Route>
-                <Route path="game2/" element={<PrivateWrapper child={<Game day={2} />} />}></Route>
-                <Route
-                    path="day0/"
-                    element={
-                        <PrivateWrapper
-                            child={
-                                <Survey
-                                    url={`https://nyu.qualtrics.com/jfe/form/SV_b3oXwg0uErHbNqK?userId=${getEncryptedUsernameFromCookie()}`}
-                                />
-                            }
-                        />
-                    }
-                ></Route>
-                <Route
-                    path="day1/"
-                    element={
-                        <PrivateWrapper
-                            child={
-                                <Survey
-                                    url={`https://nyu.qualtrics.com/jfe/form/SV_4Z9HRQoFIEaFKei?userId=${getEncryptedUsernameFromCookie()}`}
-                                />
-                            }
-                        />
-                    }
-                ></Route>
-                <Route
-                    path="writing1/"
-                    element={<PrivateWrapper child={<Writing123 day={1} content={CONTENT_WRITING_DAY1} />} />}
-                ></Route>
-                <Route
-                    path="writing2/"
-                    element={<PrivateWrapper child={<Writing123 day={2} content={CONTENT_WRITING_DAY2} />} />}
-                ></Route>
-                <Route
-                    path="writing3/"
-                    element={<PrivateWrapper child={<Writing123 day={3} content={CONTENT_WRITING_DAY3} />} />}
-                ></Route>
-                <Route
-                    path="writing6/"
-                    element={<PrivateWrapper child={<Writing6810 day={6} content={CONTENT_WRITING_DAY6} />} />}
-                ></Route>
-                <Route
-                    path="writing8/"
-                    element={<PrivateWrapper child={<Writing6810 day={8} content={CONTENT_WRITING_DAY8} />} />}
-                ></Route>
-                <Route
-                    path="writing10/"
-                    element={<PrivateWrapper child={<Writing6810 day={10} content={CONTENT_WRITING_DAY10} />} />}
-                ></Route>
-                <Route
-                    path="writing12/"
-                    element={<PrivateWrapper child={<Writing1214 day={12} content={CONTENT_WRITING_DAY12} />} />}
-                ></Route>
-                <Route
-                    path="writing14/"
-                    element={<PrivateWrapper child={<Writing1214 day={14} content={CONTENT_WRITING_DAY14} />} />}
-                ></Route>
-                <Route
-                    path="day29/"
-                    element={
-                        <PrivateWrapper
-                            child={
-                                <Survey
-                                    url={`https://nyu.qualtrics.com/jfe/form/SV_0fEtLt7hh5za4BM?userId=${getEncryptedUsernameFromCookie()}`}
-                                />
-                            }
-                        />
-                    }
-                ></Route>
-                <Route
-                    path="day45/"
-                    element={
-                        <PrivateWrapper
-                            child={
-                                <Survey
-                                    url={`https://nyu.qualtrics.com/jfe/form/SV_9TQwYhtFVafdQma?userId=${getEncryptedUsernameFromCookie()}`}
-                                />
-                            }
-                        />
-                    }
-                ></Route>
-                <Route
-                    path="day105/"
-                    element={
-                        <PrivateWrapper
-                            child={
-                                <Survey
-                                    url={`https://nyu.qualtrics.com/jfe/form/SV_4HJi8oX4onYNtPg?userId=${getEncryptedUsernameFromCookie()}`}
-                                />
-                            }
-                        />
-                    }
-                ></Route>
-                <Route path="*" element={<div>404</div>} />
-            </Routes>
-        </BrowserRouter>
-
+        <Routes>
+            <Route path="/" element={<Login/>}></Route>
+            <Route path="home/" element={<PrivateWrapper child={<Home/>}/>}></Route>
+            <Route path="game1/" element={<PrivateWrapper child={<Game day={1}/>}/>}></Route>
+            <Route path="game2/" element={<PrivateWrapper child={<Game day={2}/>}/>}></Route>
+            <Route
+                path="day0/"
+                element={
+                    <PrivateWrapper
+                        child={
+                            <Survey
+                                url={`https://nyu.qualtrics.com/jfe/form/SV_b3oXwg0uErHbNqK?userId=${getEncryptedUsernameFromCookie()}`}
+                            />
+                        }
+                    />
+                }
+            ></Route>
+            <Route
+                path="day1/"
+                element={
+                    <PrivateWrapper
+                        child={
+                            <Survey
+                                url={`https://nyu.qualtrics.com/jfe/form/SV_4Z9HRQoFIEaFKei?userId=${getEncryptedUsernameFromCookie()}`}
+                            />
+                        }
+                    />
+                }
+            ></Route>
+            <Route
+                path="writing1/"
+                element={<PrivateWrapper child={<Writing123 day={1} content={CONTENT_WRITING_DAY1}/>}/>}
+            ></Route>
+            <Route
+                path="writing2/"
+                element={<PrivateWrapper child={<Writing123 day={2} content={CONTENT_WRITING_DAY2}/>}/>}
+            ></Route>
+            <Route
+                path="writing3/"
+                element={<PrivateWrapper child={<Writing123 day={3} content={CONTENT_WRITING_DAY3}/>}/>}
+            ></Route>
+            <Route
+                path="writing6/"
+                element={<PrivateWrapper child={<Writing6810 day={6} content={CONTENT_WRITING_DAY6}/>}/>}
+            ></Route>
+            <Route
+                path="writing8/"
+                element={<PrivateWrapper child={<Writing6810 day={8} content={CONTENT_WRITING_DAY8}/>}/>}
+            ></Route>
+            <Route
+                path="writing10/"
+                element={<PrivateWrapper child={<Writing6810 day={10} content={CONTENT_WRITING_DAY10}/>}/>}
+            ></Route>
+            <Route
+                path="writing12/"
+                element={<PrivateWrapper child={<Writing1214 day={12} content={CONTENT_WRITING_DAY12}/>}/>}
+            ></Route>
+            <Route
+                path="writing14/"
+                element={<PrivateWrapper child={<Writing1214 day={14} content={CONTENT_WRITING_DAY14}/>}/>}
+            ></Route>
+            <Route
+                path="day29/"
+                element={
+                    <PrivateWrapper
+                        child={
+                            <Survey
+                                url={`https://nyu.qualtrics.com/jfe/form/SV_0fEtLt7hh5za4BM?userId=${getEncryptedUsernameFromCookie()}`}
+                            />
+                        }
+                    />
+                }
+            ></Route>
+            <Route
+                path="day45/"
+                element={
+                    <PrivateWrapper
+                        child={
+                            <Survey
+                                url={`https://nyu.qualtrics.com/jfe/form/SV_9TQwYhtFVafdQma?userId=${getEncryptedUsernameFromCookie()}`}
+                            />
+                        }
+                    />
+                }
+            ></Route>
+            <Route
+                path="day105/"
+                element={
+                    <PrivateWrapper
+                        child={
+                            <Survey
+                                url={`https://nyu.qualtrics.com/jfe/form/SV_4HJi8oX4onYNtPg?userId=${getEncryptedUsernameFromCookie()}`}
+                            />
+                        }
+                    />
+                }
+            ></Route>
+            <Route path="*" element={<div>404</div>}/>
+        </Routes>
+    </BrowserRouter>
+}
 export default App;
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,5 +1,5 @@
 import style from './Game.module.scss'
-import { getTokenFromCookie, getEncryptedUsernameFromCookie, URL } from '../../utility'
+import {getTokenFromCookie, getUserNameFromCookie, URL} from "../../utility";
 import user_avatar from './avatar/User.png'
 import supervisor_avatar from './avatar/Supervisor.png'
 import client1_avatar from './avatar/Client 1.png'
@@ -16,152 +16,67 @@ import client11_avatar from './avatar/Client 11.png'
 import client12_avatar from './avatar/Client 12.png'
 import client13_avatar from './avatar/Client 13.png'
 import client14_avatar from './avatar/Client 14.png'
-import { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import Draggable from 'react-draggable'
-import ModalDialog from 'react-bootstrap/ModalDialog'
-import React from 'react'
-import { ChatBox } from './ChatBox'
-import { TimeOutAlert } from '../TimeOutAlert/TimeOutAlert'
-
-const avatars = [
-    client1_avatar,
-    client2_avatar,
-    client3_avatar,
-    client4_avatar,
-    client5_avatar,
-    client6_avatar,
-    client7_avatar,
-    client8_avatar,
-    client9_avatar,
-    client10_avatar,
-    client11_avatar,
-    client12_avatar,
-    client13_avatar,
-    client14_avatar,
-]
+import {useEffect, useState} from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Draggable from 'react-draggable';
+import ModalDialog from 'react-bootstrap/ModalDialog';
+import React from "react";
+import {ChatBox} from "./ChatBox";
+import {TimeOutAlert} from "../TimeOutAlert/TimeOutAlert";
 
 class DraggableModalDialog extends React.Component {
     render() {
-        return (
-            <Draggable handle=".modal-title">
-                <ModalDialog {...this.props} />
-            </Draggable>
-        )
+        return <Draggable handle=".modal-title"><ModalDialog {...this.props} />
+        </Draggable>
     }
 }
 
 export interface textResponse {
-    speaker: string
-    text: string
+    speaker: string;
+    text: string;
 }
 
 export interface questionResponse extends textResponse {
-    choices: string
+    choices: string;
 }
 
 export type choicePayload = {
     choice: string
 }
 
-interface Props {
-    day: number
-}
+export function Game() {
 
-interface GuestProps {
-    guestList: Array<any>
-    setGuestList: any
-    day: number
-}
 
-const GuestList = (props: GuestProps) => {
-    const { guestList, setGuestList, day } = props
-    const updateGuestList = async () => {
-        const scenariosRes = await fetch(URL + 'api/game/scenarios/', {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + getTokenFromCookie(),
-            },
-        })
-        let scenarios = await scenariosRes.json()
-        scenarios = scenarios.scenarios
-        if (day === 1) {
-            scenarios = scenarios.filter((d: any, idx: number) => idx <= 6)
-        } else {
-            scenarios = scenarios.filter((d: any, idx: number) => idx >= 7)
-        }
-        setGuestList(scenarios)
-    }
-    useEffect(() => {
-        updateGuestList()
-    }, [])
-    return (
-        <>
-            {
-                <div className={style.clients}>
-                    {guestList.map((guest: any, idx: number) => (
-                        <div className={style.clientInfo} key={guest.name}>
-                            <div>
-                                <img
-                                    className={style.avatar}
-                                    src={avatars[guest.true_id]}
-                                    alt={`来访者${guest.order + 1}头像`}
-                                />
-                            </div>
-                            <div id={`client${guest.order + 1}`} className={style.clientName}>
-                                {guest.name}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            }
-        </>
-    )
-}
-
-export function Game(props: Props) {
-    const day = props.day
     const [chat, setChat] = useState<any>([])
-    const [score, setScore] = useState(0)
-    const [displayID, setDisplayID] = useState(0)
-    const [realID, setRealID] = useState(0)
-    const [showArr, setShowArr] = useState<boolean[]>([])
-    const visitorCount = 14
-    const [guestList, setGuestList] = useState<any>([])
+    const [score, setScore] = useState(0);
+    const [displayID, setDisplayID] = useState(0);
+    const [realID, setRealID] = useState(0);
+    const [showArr, setShowArr] = useState<boolean[]>([]);
+    const visitorCount = 14;
 
     const clientColorHighlight = () => {
         for (let i = 0; i < visitorCount; i++) {
-            const visitorElement = document.querySelector('#client' + (i + 1)) as HTMLElement
-            if (visitorElement) {
-                visitorElement.style.color = ''
-            }
+            const visitorElement = document.querySelector('#client' + (i + 1)) as HTMLElement;
+            visitorElement!.style.color = "";
         }
-        const visitorElement = document.querySelector('#client' + (displayID + 1)) as HTMLElement
-        if (visitorElement) {
-            visitorElement!.style.color = '#ff8300'
-        }
+        const visitorElement = document.querySelector('#client' + (displayID + 1)) as HTMLElement;
+        visitorElement!.style.color = "#ff8300";
     }
-    const elicitResponse = async (payload: choicePayload = { choice: '' }) => {
+    const elicitResponse = async (payload: choicePayload = {"choice": ""}) => {
+
         console.log(payload)
-        const res = await fetch(URL + 'api/game/', {
+        const res = await fetch(URL + "api/game/", {
             method: 'POST',
             headers: {
-                Accept: 'application/json',
+                'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + getTokenFromCookie(),
+                "Authorization": "Bearer " + getTokenFromCookie()
             },
             body: JSON.stringify(payload),
         })
-
         const response = await res.json()
 
-        // exit game at day 7
-        if (response.scenario_display_id === 7 && day == 1) {
-            window.location.href = '/home'
-        }
         console.log(response)
         const msgs = response.responses
         const score = response.score
@@ -180,67 +95,124 @@ export function Game(props: Props) {
         setScore(score)
         setDisplayID(newDisplayID)
         setRealID(realID)
-        setShowArr(oldArr => [...oldArr, ...newshowArr])
+        setShowArr((oldArr) => [...oldArr, ...newshowArr])
         setChat((chat: any) => [...chat, ...msgs])
+
+
+
+
     }
 
     useEffect(() => {
+
         const response = async () => {
             await elicitResponse()
         }
 
-        response().catch(e => {
+        response().catch((e) => {
             console.log(e)
         })
+
     }, [])
 
-    useEffect(() => {
+    useEffect(()=>{
         clientColorHighlight()
-        const element = document.querySelector('#chat_content')
-        element!.scrollTop = element!.scrollHeight
+        const element = document.querySelector('#chat_content');
+        element!.scrollTop = element!.scrollHeight;
         console.log('rerender')
+
     })
 
-    return (
-        <>
-            <div className={style.container}>
-                <div className={style.leftPanel}>
-                    <div className={style.userInfo}>
-                        <div className={style.avatar}>
-                            <img className={style.avatar} src={user_avatar} alt={'用户头像'} />
-                        </div>
-                        <div className={style.username}>{getEncryptedUsernameFromCookie()}</div>
+
+
+
+    return <>
+        <div className={style.container}>
+            <div className={style.leftPanel}>
+                <div className={style.userInfo}>
+
+                    <div className={style.avatar}><img className={style.avatar} src={user_avatar} alt={"用户头像"}/>
                     </div>
-                    <div className={style.score}>
-                        分数：{score}
-                        <br />第{day}部分
-                    </div>
-                    <div className={style.clientsHeader}>来访者列表</div>
-                    <GuestList guestList={guestList} setGuestList={setGuestList} day={day} />
+                    <div className={style.username}>{getUserNameFromCookie()}</div>
                 </div>
-                <div className={style.rightPanel}>
-                    <div className={style.header}>
-                        <div>{guestList[displayID - (day === 1 ? 0 : 7)]?.name}</div>
-                        <TimeOutAlert />
+                <div className={style.score}>分数：{score}</div>
+                <div className={style.clientsHeader}>来访者列表</div>
+
+                <div className={style.clients}>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client1_avatar} alt={"来访者1头像"}/></div>
+                        <div id={'client1'} className={style.clientName}>第1位来访者</div>
                     </div>
-                    <div className={style.content} id={'chat_content'}>
-                        {chat.map((c: textResponse | questionResponse, index: number) => {
-                            return (
-                                <ChatBox
-                                    showArr={showArr}
-                                    setShowArr={setShowArr}
-                                    setChat={setChat}
-                                    length={showArr.length}
-                                    elicitResponse={elicitResponse}
-                                    message={c}
-                                    index={index}
-                                    key={index}
-                                />
-                            )
-                        })}
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client2_avatar} alt={"来访者2头像"}/></div>
+                        <div id={'client2'} className={style.clientName}>第2位来访者</div>
                     </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client3_avatar} alt={"来访者3头像"}/></div>
+                        <div id={'client3'} className={style.clientName}>第3位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client4_avatar} alt={"来访者4头像"}/></div>
+                        <div id={'client4'} className={style.clientName}>第4位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client5_avatar} alt={"来访者5头像"}/></div>
+                        <div id={'client5'} className={style.clientName}>第5位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client6_avatar} alt={"来访者6头像"}/></div>
+                        <div id={'client6'} className={style.clientName}>第6位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client7_avatar} alt={"来访者7头像"}/></div>
+                        <div id={'client7'} className={style.clientName}>第7位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client8_avatar} alt={"来访者8头像"}/></div>
+                        <div id={'client8'} className={style.clientName}>第8位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client9_avatar} alt={"来访者9头像"}/></div>
+                        <div id={'client9'} className={style.clientName}>第9位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client10_avatar} alt={"来访者10头像"}/></div>
+                        <div id={'client10'} className={style.clientName}>第10位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client11_avatar} alt={"来访者11头像"}/></div>
+                        <div id={'client11'} className={style.clientName}>第11位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client12_avatar} alt={"来访者12头像"}/></div>
+                        <div id={'client12'} className={style.clientName}>第12位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client13_avatar} alt={"来访者13头像"}/></div>
+                        <div id={'client13'} className={style.clientName}>第13位来访者</div>
+                    </div>
+                    <div className={style.clientInfo}>
+                        <div><img className={style.avatar} src={client14_avatar} alt={"来访者14头像"}/></div>
+                        <div id={'client14'} className={style.clientName}>第14位来访者</div>
+                    </div>
+
+
                 </div>
             </div>
-        </>
-    )
+            <div className={style.rightPanel}>
+                <div className={style.header}>
+                    <div>第{displayID + 1}位来访者</div>
+                    <TimeOutAlert/>
+
+                </div>
+                <div className={style.content} id={'chat_content'}>
+                    {chat.map((c: textResponse | questionResponse, index: number) => {
+                        return <ChatBox showArr={showArr} setShowArr={setShowArr} setChat={setChat} length={showArr.length} elicitResponse={elicitResponse} message={c} index={index} key={index}/>
+                    })}
+                </div>
+
+            </div>
+
+        </div>
+    </>
 }
