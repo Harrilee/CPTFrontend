1f444521d637d355da8b5ff73745bc7463ef04a5
Harry Lee <harrileeeee@gmail.com>
2023-07-06 02:35:08 +0800

    feat(TaskList, Video): updated task list and video page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react'
+import Header from '../Layout/Header'
+import Footer from '../Layout/Footer'
 import style from './Home.module.scss'
 import Nav from 'react-bootstrap/Nav'
 import { Task } from '../Task/Task'
-import {getTokenFromCookie, getEncryptedUsernameFromCookie, URL, validateLogin, signOut} from '../../utility'
+import { getTokenFromCookie, getEncryptedUsernameFromCookie, URL, validateLogin, signOut } from '../../utility'
 import { TimeOutAlert } from '../TimeOutAlert/TimeOutAlert'
 import Overlay from 'react-bootstrap/Overlay'
 
@@ -63,30 +65,15 @@ export function Home() {
     }, [day, score])
 
     return (
-        <>
+        <div>
+            <Header />
             <div className={style.container}>
-                <div className={style.leftPanel}>
-                    <Nav defaultActiveKey="/home" className={`flex-column ${style.NavBar}`}>
-                        <Nav.Link href="/home" className={style.topItem}>
-                            任务列表
-                        </Nav.Link>
-                        <Nav.Link eventKey="link-1">阅读反馈</Nav.Link>
-                        <Nav.Link eventKey="link-2">联系我们</Nav.Link>
-                    </Nav>
-                    <div className={style.signOut} onClick={signOut}>
-                        <i className="fa-solid fa-right-from-bracket"></i>登出
-                    </div>
-                </div>
                 <div className={style.rightPanel}>
-                    <div className={style.appBar}>
-                        <div className={style.appBarContent}>用户ID：{getEncryptedUsernameFromCookie()}</div>
-                        <TimeOutAlert />
-                    </div>
                     <div className={style.rightContainer}>
                         <div className={style.topTitle}>欢迎回来，用户 {getEncryptedUsernameFromCookie()}</div>
                         <div className={style.topContainer}>
                             <div className={style.topCard}>
-                                今天，是您坚持参与 本次训练营的第{parseInt(day.toString())}天
+                                今天，是您坚持参与本次训练营的第{parseInt(day.toString())}天
                             </div>
                             <div className={style.topCard}>累计得分: {score}分</div>
                         </div>
@@ -122,109 +109,153 @@ export function Home() {
                             </Overlay>
                         </div>
                         <div className={style.bottomContainer}>
-                            <Task
+                            {/* <Task
                                 currentDay={day}
                                 days={0}
                                 questionType={'问卷调查'}
                                 timeToFinish={30}
                                 taskURL={'/day0'}
-                            />
+                            /> */}
                             <Task
                                 currentDay={day}
                                 days={1}
                                 questionType={'问卷调查'}
-                                timeToFinish={30}
+                                timeToFinish={20}
                                 taskURL={'/day1'}
                             />
                             <Task
                                 currentDay={day}
                                 days={1.1}
-                                questionType={'菜鸟写作'}
+                                questionType={'自由写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing1'}
                             />
                             <Task
                                 currentDay={day}
                                 days={2}
-                                questionType={'菜鸟写作'}
+                                questionType={'自由写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing2'}
                             />
                             <Task
                                 currentDay={day}
                                 days={3}
-                                questionType={'菜鸟写作'}
+                                questionType={'自由写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing3'}
                             />
                             <Task
                                 currentDay={day}
                                 days={4}
-                                questionType={'菜鸟游戏'}
+                                questionType={'科普视频'}
+                                timeToFinish={10}
+                                taskURL={'/video4'}
+                            />
+                            <Task
+                                currentDay={day}
+                                days={4.1}
+                                questionType={'游戏'}
                                 timeToFinish={60}
                                 taskURL={'/game'}
                             />
+                            <Task currentDay={day} days={5} questionType={'游戏'} timeToFinish={60} taskURL={'/game'} />
 
                             <Task
                                 currentDay={day}
                                 days={6}
-                                questionType={'菜鸟写作'}
+                                questionType={'挑战型写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing6'}
                             />
                             <Task
                                 currentDay={day}
+                                days={7}
+                                questionType={'阅读反馈'}
+                                timeToFinish={30}
+                                taskURL={'/feedback6'}
+                            />
+                            <Task
+                                currentDay={day}
                                 days={8}
-                                questionType={'菜鸟写作'}
+                                questionType={'挑战型写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing8'}
                             />
                             <Task
                                 currentDay={day}
+                                days={9}
+                                questionType={'阅读反馈'}
+                                timeToFinish={10}
+                                taskURL={'/feedback8'}
+                            />
+                            <Task
+                                currentDay={day}
                                 days={10}
-                                questionType={'菜鸟写作'}
+                                questionType={'挑战型写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing10'}
                             />
                             <Task
                                 currentDay={day}
+                                days={11}
+                                questionType={'阅读反馈'}
+                                timeToFinish={10}
+                                taskURL={'/feedback10'}
+                            />
+                            <Task
+                                currentDay={day}
                                 days={12}
-                                questionType={'菜鸟写作'}
+                                questionType={'信件写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing12'}
                             />
                             <Task
                                 currentDay={day}
+                                days={13}
+                                questionType={'阅读反馈'}
+                                timeToFinish={10}
+                                taskURL={'/feedback12'}
+                            />
+                            <Task
+                                currentDay={day}
                                 days={14}
-                                questionType={'菜鸟写作'}
+                                questionType={'信件写作'}
                                 timeToFinish={30}
                                 taskURL={'/writing14'}
                             />
                             <Task
                                 currentDay={day}
+                                days={15}
+                                questionType={'阅读反馈'}
+                                timeToFinish={10}
+                                taskURL={'/feedback14'}
+                            />
+                            <Task
+                                currentDay={day}
                                 days={29}
                                 questionType={'问卷调查'}
-                                timeToFinish={30}
+                                timeToFinish={20}
                                 taskURL={'/day29'}
                             />
                             <Task
                                 currentDay={day}
                                 days={45}
                                 questionType={'问卷调查'}
-                                timeToFinish={30}
+                                timeToFinish={20}
                                 taskURL={'/day45'}
                             />
                             <Task
                                 currentDay={day}
                                 days={105}
                                 questionType={'问卷调查'}
-                                timeToFinish={30}
+                                timeToFinish={20}
                                 taskURL={'/day105'}
                             />
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+            <Footer />
+        </div>
     )
 }
