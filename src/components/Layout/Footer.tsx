355cb68ee4e8276d572629b05da83167fdd94185
Harry Lee <harrileeeee@gmail.com>
2023-07-10 09:09:11 +0800

    feat(Footer): text change

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -10,20 +10,7 @@ export function Footer() {
                     <img src="/rainbow.png" alt="上海纽约大学压力与健康研究" className={styles.image}/>
                     <div className={styles.bannerText}>上海纽约大学压力与健康研究</div>
                 </div>
-                <div className={styles.copyright}>@ XYZ 20XX --- 20XX. All rights reserved.</div>
-            </div>
-            <div className={styles.col}>
-                <div className={styles.title}>联系我们</div>
-                <div className={styles.contact}>
-                    +86 1234567890
-                    <br/>
-                    (每周六，周日 14:00 - 16:00）
-                </div>
-                <div className={styles.contact}>
-                    <a href="mailto:shanghai.diversity.science.lab@nyu.edu">
-                        shanghai.diversity.science.lab@nyu.edu
-                    </a>
-                </div>
+                <div className={styles.copyright}>© 2023 NYU Shanghai. All rights reserved.</div>
             </div>
         </div>
     </footer>
